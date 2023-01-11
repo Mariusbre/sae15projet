d300714b24fc0c96afc2e7c8c7a018183b8f9ddc
Mariusbre <117853472+Mariusbre@users.noreply.github.com>
2023-01-11 16:18:41 +0100

    Update loto.py

--- loto.py
+++ loto.py
@@ -1,19 +1,32 @@
 # la bibliothèque numpy ne marche pas, je vais donc utiliser la bibliothèque random
 import random
-print("Entrez le nombre de tirages :")
+import matplotlib.pyplot as plt
 tirages = []
-x = int(input())
-random.seed(5)
+histo = []
+temp = 0
+x = int(input("Entrez le nombre de tirages :"))
+seed = int(input("Entrez la seed"))
+random.seed(seed)
+
 for i in range(x):
     print("tirage n°", i+1)
     for j in range(0, 5):
         n = int(random.uniform(1, 46))
         tirages.append(n)
     tirages = list(set(tirages))
-    if len(tirages) != 5:
+
+    while len(tirages) != 5:
         n = int(random.uniform(1, 46))
         tirages.append(n)
+        tirages = list(set(tirages))
+    for k in range (0, 5):
+        histo.append(tirages[k])
     print(tirages)
     tirages = []
-    print("____________")
+    print("---------------------------")
+plt.hist(histo, 45, edgecolor = "black")
+plt.xlabel("Numéros")
+plt.ylabel("Nombres de tirages")
+plt.title("Tirages du loto")
+plt.show()
 # il y'a 1221759 combinaisons possibles par tirages
